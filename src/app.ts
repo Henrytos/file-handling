88c343e7f665c5bfc0961dc72f6d9952f9b09e12
Henry <franzhenry46@gmail.com>
2024-07-05 18:19:01 -0300

    chore:init config system project

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import { fileRoutesApp} from "./http/controllers/files/route";
+import fastifyMultipart from '@fastify/multipart'
+export const app = fastify({
+    logger:true
+})
+
+app.register(fastifyMultipart)
+app.register(fileRoutesApp)
+
+
+app.listen({
+    port:3333,
+
+}).then(()=>{
+    console.log("🚀🚀🚀 Server running  🚀🚀🚀")
+})
